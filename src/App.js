222742f56174d465a599bd536f227ef02a2a129b
GorgeousGoat <gregory.drv@gmail.com>
2022-07-22 07:46:51 +0200

    font norwester & montserrat extralight

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      
+      <h1 className='fontNw'>Test</h1>
+      <h1 className='fontMst'>Test</h1>
+      <p className="fontMst"></p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './assets/fonts/Montserrat-ExtraLight.ttf';
+import './assets/fonts/norwester.otf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
 root.render(
+
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
